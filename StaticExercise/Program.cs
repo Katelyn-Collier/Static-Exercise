61e788d26a3e28cc15fe66363eb7ab1db565ac6c
Katelyn-Collier <katelyncollier14@gmail.com>
2023-01-28 15:41:59 -0600

    initial commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var c = TempConverter.FarenheitToCelsius(68);
+            var f = TempConverter.CelsiusToFarenheit(20);
+
+            Console.WriteLine("After converting:");
+            Console.WriteLine($"{c} degrees celsius");
+            Console.WriteLine($"{f} degrees farenheit");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FarenheitToCelsius(double f)
+        {
+            return (f - 32) / 1.8;
+        }
+
+        public static double CelsiusToFarenheit(double c)
+        {
+            return (c * 9) / 5 + 32;
+        }
+    }
+}
